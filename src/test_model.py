3baba1b1be566bd93ac2e492beebe9b161faa75e
SubbaraoDVS <Subbarao.Dogiparthi@gmail.com>
2026-08-23 18:49:47 +0530

    add src/test_model.py

---
+++ src/test_model.py
@@ -0,0 +1,20 @@
+
+
+import os
+import joblib
+
+
+def test_model_exists():
+
+    assert os.path.exists(
+        "/content/drive/My Drive/Colab_Notebooks/tourism_package_prediction/models/tourism_purchase_model.joblib"
+    )
+
+
+def test_model_loads():
+
+    model = joblib.load(
+        "/content/drive/My Drive/Colab_Notebooks/tourism_package_prediction/models/tourism_purchase_model.joblib"
+    )
+
+    assert model is not None
