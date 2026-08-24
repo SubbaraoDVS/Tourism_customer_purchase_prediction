5296dd95d7ed1c364d345a3c53c76e89706ac26b
SubbaraoDVS <Subbarao.Dogiparthi@gmail.com>
2026-08-24 15:38:33 +0530

    update src/test_model.py

--- src/test_model.py
+++ src/test_model.py
@@ -3,6 +3,11 @@
 import os
 import joblib
 
+BASE_DIR = Path(__file__).resolve().parent
+
+MODEL_PATH = BASE_DIR / "tourism_purchase_model.joblib"
+
+
 
 def test_model_exists():
 
@@ -13,8 +18,6 @@ def test_model_exists():
 
 def test_model_loads():
 
-    model = joblib.load(
-        "/content/drive/My Drive/Colab_Notebooks/tourism_package_prediction/src/tourism_purchase_model.joblib"
-    )
+    model = joblib.load(MODEL_PATH)
 
     assert model is not None
