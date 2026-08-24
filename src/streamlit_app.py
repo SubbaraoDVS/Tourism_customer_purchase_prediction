98a027ba38a9a8eb4259ad62f47a8fa8b5d50831
SubbaraoDVS <Subbarao.Dogiparthi@gmail.com>
2026-08-24 15:38:31 +0530

    update src/streamlit_app.py

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,14 +2,18 @@
 import streamlit as st
 import pandas as pd
 import joblib
+from pathlib import Path
+import joblib
+
+BASE_DIR = Path(__file__).resolve().parent
+
+MODEL_PATH = BASE_DIR / "tourism_purchase_model.joblib"
 
 
 @st.cache_resource
 def load_model():
 
-    return joblib.load(
-        "/content/drive/My Drive/Colab_Notebooks/tourism_package_prediction/src/tourism_purchase_model.joblib"
-    )
+    return joblib.load(MODEL_PATH)
 
 
 model = load_model()
