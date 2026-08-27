ab099acb4d8eccd379b4dbd9486e28a5d8eab8b6
SubbaraoDVS <Subbarao.Dogiparthi@gmail.com>
2026-08-27 18:34:39 +0530

    update src/test_model.py

--- src/test_model.py
+++ src/test_model.py
@@ -12,7 +12,7 @@ MODEL_PATH = BASE_DIR / "tourism_purchase_model.joblib"
 def test_model_exists():
 
     assert os.path.exists(
-        "/content/drive/My Drive/Colab_Notebooks/tourism_package_prediction/src/tourism_purchase_model.joblib"
+        "/content/drive/My Drive/Colab_Notebooks/tourism_package_prediction/models/tourism_purchase_model.joblib"
     )
 
 
