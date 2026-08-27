cc95ae84fda83dd20ba2676b49dfd43fc25837ce
SubbaraoDVS <Subbarao.Dogiparthi@gmail.com>
2026-08-27 19:25:26 +0530

    add src/Tourism_customer_purchase_prediction/src/test_model.py

---
+++ src/Tourism_customer_purchase_prediction/src/test_model.py
@@ -0,0 +1,23 @@
+
+
+import os
+import joblib
+
+BASE_DIR = Path(__file__).resolve().parent
+
+MODEL_PATH = BASE_DIR / "tourism_purchase_model.joblib"
+
+
+
+def test_model_exists():
+
+    assert os.path.exists(
+        "/content/drive/My Drive/Colab_Notebooks/tourism_package_prediction/models/tourism_purchase_model.joblib"
+    )
+
+
+def test_model_loads():
+
+    model = joblib.load(MODEL_PATH)
+
+    assert model is not None
